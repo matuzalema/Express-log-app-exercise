6fd3a7525661ef1ed2d63efeb7d7aaf56cdb394a
matuzalema <matuzalema@gmail.com>
2019-04-17 11:39:34 +0200

    Initialize project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '724824161698-2kph2kkgg9p59s9oqb356u0an2jk21lq.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'F7alpoMf1s3TR6RudXB3n0gQ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
